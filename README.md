b3e76cf2772166330c6c46244b6c960b9e99b215
svenhsia <svenhsia@gmail.com>
2017-08-25 00:16:28 +0200

    first version which allows to play with

--- README.md
+++ README.md
@@ -10,16 +10,16 @@ By using Topic Modeling method to vectorize a natural language text, we could op
 
 1. Make sure that your machine has python 3.5+ environment. Packages **gensim** and **Scikit-learn** are necessary to run this programme.
 
-2. Copy the document whose topic you want to detect into the file **todetect.txt** under the root directory of project. The text have to be in English, and no preprocessing is required.
+2. Prepare the document you want to detect, you will need to input it in the stdin or copy it in *todetect.txt* file under the root directory of project. The text has to be in English. No further preprocessing is required.
 
 3. Run the script **topicDetection.py** under the same directory. The prediction result will be printed in the standard out.
 
 ## Issues
 
-1. Due to the lack of labeled documents, the model can only project a document into one of these 4 topics cited above. Even if the document provided has nothing with these topics, it will still be predicted as one of these topics.
+1. Due to the lack of labeled documents, the model can only project a document into one of these 4 topics *alumina*, *timber*, *cement* and *paper*. Even if the document provided has nothing with these topics, it will still be predicted as one of these topics.
 
 ## TODO
 
 1. Add more labeled documents about more topics to enlarge the topic diversity of model.
 
-2. Add the fonctionality to predict a document as "UNKNOWN" when the document has a weak similarity to all topics existing in the model.
+2. Add the fonctionality of predicting a document as "UNKNOWN" when the document has a weak similarity to all topics existing in the model.
--- topicDetection.py
+++ topicDetection.py
@@ -9,16 +9,32 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 def tokenizer(doc_line):
     return [token for token in simple_preprocess(doc_line) if token not in STOPWORDS]
 
+print("We provide 4 classifiers to detect the document's topic:\nNeural Network\tSupport Vector Machine\tK Nearest Neighbors\tRandom Forest\n")
+clf = input("Choose the classifier you want to use by input one of NN, SVM, KNN and RF:")
+clf_name = {"NN": "Neural Network", "SVM": "Support Vector Machine", "KNN": "K Nearest Neighbors", "RF": "Random Forest"}
+
+while clf not in clf_name:
+    clf = input("\nPlease choose from NN, SVM, KNN and RF:")
+
+print("\nYou have choosen {}.".format(clf_name[clf]))
+
 text = ''
-with open('todetect.txt', 'r') as f:
-    for line in f:
-        text = text + line
-text = text.replace('\n', ' ').replace('\r', ' ')
+doc_input = input("\nPlease input your document as a single line string,\nor press ENTER if you have copied it into todetect.txt:")
+
+if doc_input == '':
+    with open('todetect.txt', 'r') as f:
+        for line in f:
+            text = text + line
+    text = text.replace('\n', ' ').replace('\r', ' ')
+else:
+    text = doc_input
+
 doc = tokenizer(text)
 
 dictionary = corpora.Dictionary.load('src/word2id.dict')
 tfidf = models.TfidfModel.load('src/model.tfidf')
 lsi = models.LsiModel.load('src/model.lsi')
+clf = joblib.load('src/{}.pkl'.format(clf))
 
 corpus_test = dictionary.doc2bow(doc)
 corpus_tf = tfidf[corpus_test]
@@ -26,12 +42,7 @@ corpus_lsi = lsi[corpus_tf]
 vec_lsi = [t[1] for t in corpus_lsi]
 vec_norm = normalize([vec_lsi])[0]
 
-
-# clf = joblib.load('src/svm.pkl')
-clf = joblib.load('src/nn.pkl')
-# clf = joblib.load('src/knn.pkl')
-# clf = joblib.load('src/rf.pkl')
 y_predict = clf.predict([vec_norm])[0]
 label_dict = {0:'alumina', 1: 'timber', 2: 'cement', 3: 'paper'}
 label = label_dict[y_predict]
-print(label)
+print('\nYour document talks about "{}".\n'.format(label))
